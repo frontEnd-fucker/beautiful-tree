7a9d6b9bc04e159cce1a15a1e2c25bc43d589f07
Mogrea <yw@YdeMacBook-Pro.local>
2023-12-20 12:04:07 +0800

    init eslint

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -5,9 +5,13 @@ export type ButtonProps = {
   size?: "xs" | "sm" | "md" | "lg" | "xl";
   /** button content */
   children: ReactNode;
+  color: string;
 };
 
 export const Button: React.FC<ButtonProps> = ({ size = "md", children }) => {
+  const color = "";
+  color = 1;
+
   return (
     <div>
       <div>{children}</div>
